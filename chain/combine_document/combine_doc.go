60c4c73a6a3bbf9eaad9586eb36accd47a2a818c
wejick <wejick@gmail.com>
2023-05-19 23:00:19 +0700

    ADD stuff_combined_document chain

---
+++ chain/combine_document/combine_doc.go
@@ -0,0 +1,70 @@
+package combine_document
+
+import (
+	"context"
+	"errors"
+
+	"github.com/wejick/gochain/chain"
+	"github.com/wejick/gochain/chain/llm_chain"
+	"github.com/wejick/gochain/model"
+	"github.com/wejick/gochain/prompt"
+)
+
+type CombinedDocument interface {
+	// Combine document and do something
+	// the implementation of combine can run LLM againts the doc
+	Combine(ctx context.Context, docs []string, options ...func(*model.Option)) (output string, err error)
+}
+
+var _ CombinedDocument = &StuffCombineDocument{}
+var _ chain.BaseChain = &StuffCombineDocument{}
+
+// CombineDocument chain to work on text document with defined prompt
+type StuffCombineDocument struct {
+	prompt            *prompt.Prompt_template
+	llmChain          *llm_chain.LLMChain
+	promptTemplateKey string
+}
+
+func NewStuffCombineDocument(prompt *prompt.Prompt_template,
+	templateKey string, llmChain *llm_chain.LLMChain) *StuffCombineDocument {
+	return &StuffCombineDocument{
+		prompt:            prompt,
+		promptTemplateKey: templateKey,
+		llmChain:          llmChain,
+	}
+}
+
+func (S *StuffCombineDocument) Combine(ctx context.Context, docs []string, options ...func(*model.Option)) (output string, err error) {
+	//concat all docs into 1 string
+	var doc string
+	for _, item := range docs {
+		doc += item + "\n"
+	}
+	templateData := map[string]string{S.promptTemplateKey: doc}
+
+	prompt, err := S.prompt.FormatPrompt(templateData)
+	if err != nil {
+		return
+	}
+	output, err = S.llmChain.SimpleRun(ctx, prompt)
+
+	return
+}
+
+// Run expect input["input"] as input, and put the result to output["output"]
+func (S *StuffCombineDocument) Run(ctx context.Context, input map[string]string, options ...func(*model.Option)) (output map[string]string, err error) {
+	if _, ok := input["input"]; !ok {
+		return output, errors.New("input[\"input\"] is not specified")
+	}
+	output = make(map[string]string)
+	output["output"], err = S.Combine(ctx, []string{input["input"]})
+
+	return
+}
+
+// SimpleRun will run the input string agains llmchain
+func (S *StuffCombineDocument) SimpleRun(ctx context.Context, input string, options ...func(*model.Option)) (output string, err error) {
+	output, err = S.Combine(ctx, []string{input})
+	return
+}
---
+++ chain/combine_document/combine_doc_test.go
@@ -0,0 +1,189 @@
+package combine_document
+
+import (
+	"context"
+	"reflect"
+	"testing"
+
+	"github.com/wejick/gochain/chain/llm_chain"
+	"github.com/wejick/gochain/model"
+	"github.com/wejick/gochain/prompt"
+)
+
+var emptyPrompt, _ = prompt.NewPromptTemplate("empty", "")
+var echoPrompt, _ = prompt.NewPromptTemplate("empty", "{{.echo}}")
+var echoLlmChain = llm_chain.NewLLMChain(&model.LLMModelMock{
+	CallFunc: func(ctx context.Context, prompt string, options ...func(*model.Option)) (string, error) {
+		return prompt, nil
+	},
+})
+
+func TestStuffCombineDocument_Combine(t *testing.T) {
+
+	type fields struct {
+		prompt            *prompt.Prompt_template
+		llmChain          *llm_chain.LLMChain
+		promptTemplateKey string
+	}
+	type args struct {
+		ctx     context.Context
+		docs    []string
+		options []func(*model.Option)
+	}
+	tests := []struct {
+		name       string
+		fields     fields
+		args       args
+		wantOutput string
+		wantErr    bool
+	}{
+		{
+			name: "empty",
+			fields: fields{
+				prompt:   emptyPrompt,
+				llmChain: echoLlmChain,
+			},
+		},
+		{
+			name: "crowded, jakarta",
+			fields: fields{
+				prompt:            echoPrompt,
+				llmChain:          echoLlmChain,
+				promptTemplateKey: "echo",
+			},
+			args: args{
+				ctx:  context.Background(),
+				docs: []string{"crowded", "jakarta"},
+			},
+			wantOutput: "crowded\njakarta\n",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			S := &StuffCombineDocument{
+				prompt:            tt.fields.prompt,
+				llmChain:          tt.fields.llmChain,
+				promptTemplateKey: tt.fields.promptTemplateKey,
+			}
+			gotOutput, err := S.Combine(tt.args.ctx, tt.args.docs, tt.args.options...)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("StuffCombineDocument.Combine() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if gotOutput != tt.wantOutput {
+				t.Errorf("StuffCombineDocument.Combine() = %v, want %v", gotOutput, tt.wantOutput)
+			}
+		})
+	}
+}
+
+func TestStuffCombineDocument_Run(t *testing.T) {
+	type fields struct {
+		prompt            *prompt.Prompt_template
+		llmChain          *llm_chain.LLMChain
+		promptTemplateKey string
+	}
+	type args struct {
+		ctx     context.Context
+		input   map[string]string
+		options []func(*model.Option)
+	}
+	tests := []struct {
+		name       string
+		fields     fields
+		args       args
+		wantOutput map[string]string
+		wantErr    bool
+	}{
+		{
+			name:    "empty",
+			wantErr: true,
+		},
+		{
+			name: "crowded,jakarta",
+			fields: fields{
+				prompt:            echoPrompt,
+				llmChain:          echoLlmChain,
+				promptTemplateKey: "echo",
+			},
+			args: args{
+				input: map[string]string{"input": "crowded,jakarta"},
+			},
+			wantOutput: map[string]string{"output": "crowded,jakarta\n"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			S := &StuffCombineDocument{
+				prompt:            tt.fields.prompt,
+				llmChain:          tt.fields.llmChain,
+				promptTemplateKey: tt.fields.promptTemplateKey,
+			}
+			gotOutput, err := S.Run(tt.args.ctx, tt.args.input, tt.args.options...)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("StuffCombineDocument.Run() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(gotOutput, tt.wantOutput) {
+				t.Errorf("StuffCombineDocument.Run() = %v, want %v", gotOutput, tt.wantOutput)
+			}
+		})
+	}
+}
+
+func TestStuffCombineDocument_SimpleRun(t *testing.T) {
+	type fields struct {
+		prompt            *prompt.Prompt_template
+		llmChain          *llm_chain.LLMChain
+		promptTemplateKey string
+	}
+	type args struct {
+		ctx     context.Context
+		input   string
+		options []func(*model.Option)
+	}
+	tests := []struct {
+		name       string
+		fields     fields
+		args       args
+		wantOutput string
+		wantErr    bool
+	}{
+		{
+			name: "empty",
+			fields: fields{
+				prompt:   emptyPrompt,
+				llmChain: echoLlmChain,
+			},
+		},
+		{
+			name: "crowded,jakarta",
+			fields: fields{
+				prompt:            echoPrompt,
+				llmChain:          echoLlmChain,
+				promptTemplateKey: "echo",
+			},
+			args: args{
+				input: "crowded,jakarta",
+			},
+			wantOutput: "crowded,jakarta\n",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			S := &StuffCombineDocument{
+				prompt:            tt.fields.prompt,
+				llmChain:          tt.fields.llmChain,
+				promptTemplateKey: tt.fields.promptTemplateKey,
+			}
+			gotOutput, err := S.SimpleRun(tt.args.ctx, tt.args.input, tt.args.options...)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("StuffCombineDocument.SimpleRun() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if gotOutput != tt.wantOutput {
+				t.Errorf("StuffCombineDocument.SimpleRun() = %v, want %v", gotOutput, tt.wantOutput)
+			}
+		})
+	}
+}
