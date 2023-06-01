ff0431d1ef34751fd1950661d9eec16bc9799741
wejick <wejick@gmail.com>
2023-06-01 19:21:30 +0700

    add chat model integration test to sum chain

--- chain/integration_test/integration_test.go
+++ chain/integration_test/integration_test.go
@@ -17,6 +17,7 @@ import (
 )
 
 var llmModel model.LLMModel
+var chatModel model.LLMModel
 
 func TestMain(m *testing.M) {
 	fmt.Println("Running integration tests...")
@@ -24,6 +25,8 @@ func TestMain(m *testing.M) {
 
 	var authToken = os.Getenv("OPENAI_API_KEY")
 	llmModel = _openai.NewOpenAIModel(authToken, "", "text-davinci-003")
+
+	chatModel = _openai.NewOpenAIChatModel(authToken, "", _openai.GPT3Dot5Turbo0301)
 
 	exitCode := m.Run()
 
@@ -75,4 +78,19 @@ func TestMapReduceSummarizationChain(t *testing.T) {
 
 	t.Log(output)
 
+}
+
+func TestStuffSummarizationChainChat(t *testing.T) {
+	llmchain := llm_chain.NewLLMChain(chatModel)
+	chain, err := summarization.NewStuffSummarizationChain(llmchain, "", "text")
+	assert.NoError(t, err, "error NewStuffSummarizationChain")
+	output, err := chain.SimpleRun(context.Background(), `Modular audio and video hardware for retro machines like the Commodore 64. Designed to use 74 series TTL through hole ICs available back in the 1980s, something you can solder at home from parts or order ready assembled.
+	One of the most recent videos shows a "Shadow of the Beast" demonstration, to show parallax scrolling with precisely timed raster effects. Please do consider subscribing to the YouTube channel if you want to see more updates to this project: 
+	This project started when old retro arcade hardware was being discussed. In the back of my mind was the often fabled "Mega games" by Imagine Software which were planned to use extra hardware on the Spectrum and Commodore 64 to augment the machine's capabilities. Since this hardware uses TTL logic available back from the same time period I was wondering exactly how much extra graphical grunt could have been engineered and interfaced with these old 8-bit computers.
+	Truth be told, the Imagine hardware was pretty much just extra RAM https://www.gamesthatwerent.com/gtw64/mega-games/ but this was a fun project to see how far the arcade hardware was pushing the limits of board size and signal complexity.
+	I was looking at Bomb Jack boards on ebay and pondering how they had enough fill-rate to draw 24 16x16 sprites and have the option for some to use 32x32 mode as well. A friend and I were discussing the clock speed and fill-rate while trying to deduce the operation of the hardware just by inspecting the hand drawn schematics, as you do.
+	In the end to get some clarity on the sprite plotting specifically I started to transcribe what was thought to be the sprite logic portion of the schematic into Proteus, since it can simulate digital electronics really well.`)
+
+	assert.NoError(t, err)
+	t.Log(output)
 }
