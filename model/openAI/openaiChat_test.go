37248cd85c5dd0aaa4f3d0ec262d27cea888d500
wejick <wejick@gmail.com>
2023-07-02 12:06:43 +0700

    add test case for message conversion

--- model/openAI/openaiChat_test.go
+++ model/openAI/openaiChat_test.go
@@ -23,12 +23,16 @@ func TestConvertMessageToOai(t *testing.T) {
 func TestConvertMessagesToOai(t *testing.T) {
 	messages := []model.ChatMessage{
 		{Role: "system", Content: "Welcome to our system"},
+		{Role: "AI", Content: "Heiho, I am AI"},
 		{Role: "user", Content: "Hello, I need assistance"},
+		{Role: "AI", FunctionName: "assist", Content: "I will assist you"},
 	}
 
 	expected := []openai.ChatCompletionMessage{
 		{Role: "system", Content: "Welcome to our system"},
+		{Role: "AI", Content: "Heiho, I am AI"},
 		{Role: "user", Content: "Hello, I need assistance"},
+		{Role: "AI", Name: "assist", Content: "I will assist you"},
 	}
 
 	result := convertMessagesToOai(messages)
@@ -47,7 +51,53 @@ func Test_convertOaiMessageToChat(t *testing.T) {
 		args args
 		want model.ChatMessage
 	}{
-		// TODO: Add test cases.
+		{
+			name: "empty message",
+		},
+		{
+			name: "no function system",
+			args: args{
+				chatMessage: goopenai.ChatCompletionMessage{
+					Role:    "system",
+					Content: "Hello, I need assistance",
+				},
+			},
+			want: model.ChatMessage{
+				Role:    model.ChatMessageRoleSystem,
+				Content: "Hello, I need assistance",
+			},
+		},
+		{
+			name: "no function Ai",
+			args: args{
+				chatMessage: goopenai.ChatCompletionMessage{
+					Role:    "assistant",
+					Content: "Hello, I need assistance",
+				},
+			},
+			want: model.ChatMessage{
+				Role:    model.ChatMessageRoleAssistant,
+				Content: "Hello, I need assistance",
+			},
+		},
+		{
+			name: "need function",
+			args: args{
+				chatMessage: goopenai.ChatCompletionMessage{
+					Role: "assistant",
+					FunctionCall: &goopenai.FunctionCall{
+						Name:      "test",
+						Arguments: "argument here",
+					},
+				},
+			},
+			want: model.ChatMessage{
+				Role:          model.ChatMessageRoleAssistant,
+				Content:       "",
+				FunctionName:  "test",
+				ParameterJson: "argument here",
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
