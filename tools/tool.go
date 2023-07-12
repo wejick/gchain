bf4bdcd8f1b33c1ca36fb2ce3663b5da694c7fa1
wejick <wejick@gmail.com>
2023-07-12 10:20:22 +0700

    more explanation on basetool

--- tools/tool.go
+++ tools/tool.go
@@ -1,16 +1,19 @@
 package tools
 
 import (
+	"context"
 	"fmt"
 
-	"github.com/wejick/gchain/chain"
 	"github.com/wejick/gchain/model"
 )
 
 // BaseTool is the interface for tool
 // the idea is to keep it compatible with chain interface, so chain be used as tool as well
 type BaseTool interface {
-	chain.BaseChain
+	// Run expect map string of string, each tool may expect different data
+	Run(ctx context.Context, prompt map[string]string, options ...func(*model.Option)) (output map[string]string, err error)
+	// SimpleRun expect valid json string, each tool may expect different data
+	SimpleRun(ctx context.Context, prompt string, options ...func(*model.Option)) (output string, err error)
 	GetFunctionDefinition() model.FunctionDefinition // Get tools definition in the form of function definition
 	GetDefinitionString() string                     // Get tools definition in the form of text description
 }
